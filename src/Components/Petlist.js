df85276f504f2891319d265f2cdde227156b6fb6
anwarismail <anwaarismail449@gmail.com>
2022-10-13 09:28:58 +0300

    CReate and import components

---
+++ src/Components/Petlist.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Forms from './Forms'
+import PetCard from './PetCard'
+
+function Petlist() {
+  return (
+    <div>
+      <Forms/>
+      <PetCard/>
+    </div>
+  )
+}
+
+export default Petlist
